156ec14aa578975309dacd7f52a9cee9068e44c7
Elguncode <elgunmb@code.edu.az>
2025-02-14 03:15:23 +0100

    language logic is added

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
   base: "/", // Ensure base is set correctly
   assetsInclude: ["**/*.woff", "**/*.woff2", "**/*.ttf"],
 });
