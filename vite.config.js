b74a0953b47276c82668156341dffa3b097c42ee
Elguncode <elgunmb@code.edu.az>
2025-02-14 03:17:11 +0100

    for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
+  plugins: [react()],
   base: "/", // Ensure base is set correctly
   assetsInclude: ["**/*.woff", "**/*.woff2", "**/*.ttf"],
 });
